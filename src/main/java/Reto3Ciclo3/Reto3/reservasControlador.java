5a16b57340da2a4aa8064bc33853a5869ef71808
jenni cortez <jenni.cortez.mt@correo.usa.edu.co>
2021-10-31 11:25:44 -0500

    puse carpeta static

--- src/main/java/Reto3Ciclo3/Reto3/reservasControlador.java
+++ src/main/java/Reto3Ciclo3/Reto3/reservasControlador.java
@@ -1,4 +1,4 @@
-package Reto3Ciclo3.Reto3;
+    package Reto3Ciclo3.Reto3;
 
 import java.util.List;
 import java.util.Optional;
---
+++ src/main/java/Reto3Ciclo3/Reto3/security/securityAdapter.java
@@ -0,0 +1,35 @@
+package Reto3Ciclo3.Reto3.security;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class securityAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> {
+            try {
+                a
+                        .antMatchers("/", "/error", "/webjars/**",
+                                "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                        .and().logout().logoutSuccessUrl("/").permitAll();
+            } catch (Exception ex) {
+                Logger.getLogger(securityAdapter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        ).exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        ).oauth2Login().defaultSuccessUrl("/", true);
+
+        http.cors().and().csrf().disable();
+
+    }
+
+}
